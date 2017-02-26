e178000fbbf5df186240bdbbbf628614238e1d7d
Samuel <samjsong@mit.edu>
2017-02-26 11:48:34 -0500

    Add anthill files

---
+++ lab_1_anteater/Anteater/SensorModel.swift
@@ -0,0 +1,103 @@
+//
+//  SensorModel.swift
+//  Anteater
+//
+//  Created by Justin Anderson on 8/1/16.
+//  Copyright © 2016 MIT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreBluetooth
+
+protocol SensorModelDelegate {
+    func sensorModel(_ model: SensorModel, didChangeActiveHill hill: Hill?)
+    func sensorModel(_ model: SensorModel, didReceiveReadings readings: [Reading], forHill hill: Hill?)
+}
+
+extension Notification.Name {
+    public static let SensorModelActiveHillChanged = Notification.Name(rawValue: "SensorModelActiveHillChangedNotification")
+    public static let SensorModelReadingsChanged = Notification.Name(rawValue: "SensorModelHillReadingsChangedNotification")
+}
+
+enum ReadingType: Int {
+    case Unknown = -1
+    case Humidity = 2
+    case Temperature = 1
+    case Error = 0
+}
+
+struct Reading {
+    let type: ReadingType
+    let value: Double
+    let date: Date = Date()
+    let sensorId: String?
+    
+    func toJson() -> [String: Any] {
+        return [
+            "value": self.value,
+            "type": self.type.rawValue,
+            "timestamp": self.date.timeIntervalSince1970,
+            "userid": UIDevice.current.identifierForVendor?.uuidString ?? "NONE",
+            "sensorid": sensorId ?? "NONE"
+        ]
+    }
+}
+
+extension Reading: CustomStringConvertible {
+    var description: String {
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        guard let numberString = formatter.string(from: NSNumber(value: self.value)) else {
+            print("Double \"\(value)\" couldn't be formatted by NumberFormatter")
+            return "NaN"
+        }
+        switch type {
+        case .Temperature:
+            return "\(numberString)°F"
+        case .Humidity:
+            return "\(numberString)%"
+        default:
+            return "\(type)"
+        }
+    }
+}
+
+struct Hill {
+    var readings: [Reading]
+    var name: String
+    
+    init(name: String) {
+        readings = []
+        self.name = name
+    }
+}
+
+extension Hill: CustomStringConvertible, Hashable, Equatable {
+    var description: String {
+        return name
+    }
+    
+    var hashValue: Int {
+        return name.hashValue
+    }
+}
+
+func ==(lhs: Hill, rhs: Hill) -> Bool {
+    return lhs.name == rhs.name
+}
+
+class SensorModel {
+    
+    static let kBLE_SCAN_TIMEOUT = 10000.0
+    
+    static let shared = SensorModel()
+
+    var delegate: SensorModelDelegate?
+    var sensorReadings: [ReadingType: [Reading]] = [.Humidity: [], .Temperature: []]
+    var activeHill: Hill?
+    
+    init() {
+    }
+}
